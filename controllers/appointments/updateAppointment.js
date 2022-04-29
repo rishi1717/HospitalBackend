f0ad1ee1e37255e81a019c5a99d800db19208aad
rishi1717 <rshrrrss@gmail.com>
2022-04-29 10:23:09 +0530

    appointment update controller added

--- controllers/appointments/updateAppointment.js
+++ controllers/appointments/updateAppointment.js
@@ -1,3 +1,14 @@
-export async function updateAppointment (req, res) {
-	res.send("updated Department")
+import Appointments from "../../models/appointmentModel.js"
+
+export async function updateAppointment(req, res) {
+	try {
+		console.log(req.params.id)
+		console.log(req.body)
+		await Appointments.updateOne({ _id: req.params.id }, {$set:{...req.body}})
+		const appointment = await Appointments.find({ _id: req.params.id })
+		res.status(201).send({ appointment, message: "Appointment Updated succesfully" })
+	} catch (err) {
+		console.log(err.message)
+		res.status(500).send({ message: err.message })
+	}
 }
