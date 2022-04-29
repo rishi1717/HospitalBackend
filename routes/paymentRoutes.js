b16ba2f8cbc095b0672d83848d84d0f256e21185
rishi1717 <rshrrrss@gmail.com>
2022-04-29 12:23:02 +0530

    authVerify added to payment

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -4,8 +4,9 @@ const router = Router()
 import { addPayment } from "../controllers/payments/addPayment.js"
 import { getPayments } from "../controllers/payments/getPayments.js"
 import { updatePayment } from "../controllers/payments/updatePayment.js"
+import authVerify from '../middlewares/authVerify.js'
 
-router.post("/", addPayment)
+router.post("/",authVerify, addPayment)
 
 router.get("/:id?", getPayments)
 
