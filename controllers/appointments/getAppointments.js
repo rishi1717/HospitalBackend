b199fca13c0bcdac9f53adafa0aab4e065fca127
rishi1717 <rshrrrss@gmail.com>
2022-05-07 10:15:48 +0530

    sort filter appointments

--- controllers/appointments/getAppointments.js
+++ controllers/appointments/getAppointments.js
@@ -3,7 +3,9 @@ import Appointment from "../../models/appointmentModel.js"
 export async function getAppointments(req, res) {
 	try {
 		if (req.params.id) {
-			let appointment = await Appointment.find({ userId: req.params.id })
+			let appointment = await Appointment.find({
+				userId: req.params.id,
+			}).sort({ date: -1, time: -1 })
 			res.status(201).send({ appointment, message: "appointment found" })
 		} else {
 			let appointment = await Appointment.find().sort({ date: -1, time: -1 })
