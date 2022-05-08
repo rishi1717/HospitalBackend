d7b2606532a9106ecb48dfda824dbca4189fa065
rishi1717 <rshrrrss@gmail.com>
2022-05-08 18:35:57 +0530

    reschedule done

--- controllers/appointments/updateAppointment.js
+++ controllers/appointments/updateAppointment.js
@@ -1,8 +1,11 @@
 import Appointment from "../../models/appointmentModel.js"
+import dayjs from "dayjs"
 
 export async function updateAppointment(req, res) {
 	try {
-		await Appointment.updateOne({ _id: req.params.id }, {$set:{...req.body}})
+		console.log(req.body)
+		const date = dayjs(req.body.date).format("DD/MM/YYYY")
+		await Appointment.updateOne({ _id: req.params.id }, {$set:{...req.body, date: date}})
 		const appointment = await Appointment.find({ _id: req.params.id })
 		res.status(201).send({ appointment, message: "Appointment Updated succesfully" })
 	} catch (err) {
