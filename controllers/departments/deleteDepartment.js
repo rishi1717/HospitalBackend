cbdbc5e4dcfef33b64f697788f0f9d318573d5c5
rishi1717 <rshrrrss@gmail.com>
2022-05-12 14:40:27 +0530

    department delete

--- controllers/departments/deleteDepartment.js
+++ controllers/departments/deleteDepartment.js
@@ -1,3 +1,11 @@
-export async function deleteDepartment (req, res) {
-	res.send(req.params.id)
+import Department from "../../models/departmentModel.js"
+
+export async function deleteDepartment(req, res) {
+	try{
+		await Department.findOneAndDelete({ _id: req.params.id })
+		res.status(201).send({ message: "department deleted succesfully" })
+	}catch(err){
+		console.log(err.message)
+		res.status(500).json({ message: err.message })
+	}
 }
