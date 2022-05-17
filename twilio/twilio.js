73348d6b313b413820ec37347db31b003ae405a2
rishi1717 <rshrrrss@gmail.com>
2022-05-17 20:35:53 +0530

    otp update 3

--- twilio/twilio.js
+++ twilio/twilio.js
@@ -6,15 +6,15 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
 const serviceSid = process.env.TWILIO_SERVICE_ID
 
-export const sendSms = (phone, message) => {
+export const sendSms = (phone, otp) => {
 	console.log(phone)
 	const client = new twilio(accountSid, authToken)
-	client.verify.services(serviceSid).verificationChecks
-		.create({
-			body: message,
+	client.verify
+		.services(serviceSid)
+		.verificationChecks.create({
 			from: process.env.TWILIO_PHONE_NUMBER,
 			to: phone,
+			code: otp,
 		})
 		.then((message) => console.log(message.sid))
 }
-
