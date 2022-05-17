9f869a0628c11ed2fb4a0d6678f35fce5ccafd66
rishi1717 <rshrrrss@gmail.com>
2022-05-17 20:39:25 +0530

    otp revert

--- twilio/twilio.js
+++ twilio/twilio.js
@@ -4,17 +4,15 @@ dotenv.config()
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID
 const authToken = process.env.TWILIO_AUTH_TOKEN
-const serviceSid = process.env.TWILIO_SERVICE_ID
 
-export const sendSms = (phone, otp) => {
-	console.log(phone)
+export const sendSms = (phone, message) => {
 	const client = new twilio(accountSid, authToken)
-	client.verify
-		.services(serviceSid)
-		.verificationChecks.create({
+	client.messages
+		.create({
+			body: message,
 			from: process.env.TWILIO_PHONE_NUMBER,
 			to: phone,
-			code: otp,
 		})
 		.then((message) => console.log(message.sid))
 }
+
