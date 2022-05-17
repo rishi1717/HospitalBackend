46101a281b88744ce5619bb37da93aac139be1d6
rishi1717 <rshrrrss@gmail.com>
2022-05-17 11:41:46 +0530

    caps

--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -2,9 +2,9 @@ import { Router } from "express"
 const router = Router()
 
 import { addDepartment } from "../controllers/departments/addDepartment.js"
-import { getDepartments } from "../controllers/departments/getdepartments.js"
-import { updateDepartment } from "../controllers/departments/updatedepartment.js"
-import { deleteDepartment } from "../controllers/departments/deletedepartment.js"
+import { getDepartments } from "../controllers/departments/getDepartments.js"
+import { updateDepartment } from "../controllers/departments/updateDepartment.js"
+import { deleteDepartment } from "../controllers/departments/deleteDepartment.js"
 import authVerify from "../middlewares/authVerify.js"
 
 router.post("/", authVerify, addDepartment)
