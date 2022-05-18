1a3b23365480b9da25e63fb246048d220de29f6d
rishi1717 <rshrrrss@gmail.com>
2022-05-18 08:54:23 +0530

    otp retry 6

--- twilio/twilio.js
+++ twilio/twilio.js
@@ -9,11 +9,12 @@ const serviceId = process.env.TWILIO_SERVICE_SID
 export const sendSms = (phone, otp) => {
 	const client = new twilio(accountSid, authToken)
 
+
 	try {
 		console.log(accountSid, authToken, serviceId, phone, otp)
 		client.verify
 			.services(serviceId)
-			.verificationChecks.create({ to: phone, code: otp, channel: "sms" })
+			.verifications.create({ to: phone, code: otp, channel: "sms" })
 			.then((verification_check) => console.log(verification_check.status))
 			.catch((err) => {
 				console.log(err)
