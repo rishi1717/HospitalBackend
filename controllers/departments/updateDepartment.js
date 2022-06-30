b9bdd52d2a146f96a280fc79fe76762973e50e22
Rishi Releesh <67540263+rishi1717@users.noreply.github.com>
2022-06-30 11:10:17 +0530

    Update updateDepartment.js

--- controllers/departments/updateDepartment.js
+++ controllers/departments/updateDepartment.js
@@ -1,3 +1,16 @@
-export async function updateDepartment (req, res) {
-	res.send("updated Department")
+import Department from "../../models/departmentModel.js"
+
+export async function updateDepartment(req, res) {
+	try {
+
+		if (req.userjwt.role === "doctor" || req.userjwt.role === "admin") {
+			await Department.updateOne({ _id: req.params.id }, { $set: { name: req.body.name } })
+			res.status(201).send({ message: "department Updated succesfully" })
+		} else {
+			res.status(401).send({ message: "unauthorized" })
+		}
+	} catch (err) {
+		console.log(err.message)
+		res.status(500).json({ message: err.message })
+	}
 }
